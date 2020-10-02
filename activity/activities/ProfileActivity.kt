d83449ebbf4700fcc7aed72848b2887e595e6e87
sohamD07 <63590114+sohamD07@users.noreply.github.com>
2020-10-02 16:26:07 +0530

    Add files via upload

---
+++ activity/activities/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.soham.foodcare.activity.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.soham.foodcare.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
---
+++ activity/model/RestaurantMenu.kt
@@ -0,0 +1,7 @@
+package com.soham.foodcare.activity.model
+
+data class RestaurantMenu (
+    var id:String,
+    var name:String,
+    var cost_for_one:String
+)
